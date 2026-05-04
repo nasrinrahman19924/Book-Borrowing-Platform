24c83d570c55fd02509d5a53ea07ac0f0089b485
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-05-04 10:35:29 +0600

    final modify

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,5 +1,3 @@
-import { auth } from "@/lib/auth";
-
-const handler = auth.handler;
-
-export { handler as GET, handler as POST };
+import { auth } from "@/lib/auth"; // path to your auth file
+import { toNextJsHandler } from "better-auth/next-js";
+export const { POST, GET } = toNextJsHandler(auth);
