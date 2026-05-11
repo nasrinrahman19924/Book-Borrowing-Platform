580a70b39de0a714e453b2145c9bef83f67505f3
nasrinrahman19924 <nasrinrahman19924@gmail.com>
2026-05-11 17:18:14 +0600

    modify my assignment

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -10,26 +10,51 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("book-borrowing-platform");
 
-
 export const auth = betterAuth({
-   experimental: { joins: true },
-
-  emailAndPassword: {
-    enabled: true,
-  },
-
-
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-  baseURL: process.env.BETTER_AUTH_URL,
-  secret: process.env.BETTER_AUTH_SECRET,
-
+  emailAndPassword: {
+    enabled: true
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
-  },
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      
+    }
+  }
 });
+
+
+
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+// const client = new MongoClient(process.env.MONGODB_URI);
+// const db = client.db("book-borrowing-platform");
+
+
+// export const auth = betterAuth({
+//    experimental: { joins: true },
+
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+
+
+//   database: mongodbAdapter(db, {
+//     // Optional: if you don't provide a client, database transactions won't be enabled.
+//     client
+//   }),
+//   baseURL: process.env.BETTER_AUTH_URL,
+//   secret: process.env.BETTER_AUTH_SECRET,
+
+//   socialProviders: {
+//     google: {
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     },
+//   },
+// });
